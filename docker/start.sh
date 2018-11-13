b987971fe871b9310de8027b7dbaa4aa9e9adb5a
Victor <vli02@hotmail.com>
2018-11-13 00:15:38 -0800

    add command to exec to toolchain.

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-bin_dir=$(dirname "$0")
+did=$(docker ps --filter "ancestor=toolchain:alpine" -q)
 
-cd "$bin_dir"
-
-top_dir="$(git rev-parse --show-toplevel)"
-
-docker run --init -i$([ -t 0 ] && echo t) -u $(id -u):$(id -g) -v "$top_dir":/inthsmp -w /inthsmp toolchain:alpine
+if [ -z $did ]; then
+    echo "Start toolchain container."
+    bin_dir=$(dirname "$0")
+    cd "$bin_dir"
+    top_dir="$(git rev-parse --show-toplevel)"
+    docker run --init -i$([ -t 0 ] && echo t) -u $(id -u):$(id -g) -v "$top_dir":/inthsmp -w /inthsmp toolchain:alpine
+    echo "Terminat toolchain container."
+else
+    echo "Exec into toolchain container."
+    docker exec -i$([ -t 0 ] && echo t) -u $(id -u):$(id -g) -w /inthsmp $did /bin/sh -l
+    echo "Exit toolchain container."
+fi
