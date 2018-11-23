cc974f586472f428ebad7f9f67ad907d13b0bf4d
Victor <vli02@hotmail.com>
2018-11-23 13:05:48 -0800

    fix BaseState class.

--- lib/python/inthsm/inthsm.py
+++ lib/python/inthsm/inthsm.py
@@ -10,55 +10,55 @@ def HHAccept():
 def HHAbort():
     raise HSMAbortException
 
-def __HHGuardFalse():
-    return False
+def HHGuardTrue():
+    return True
 
-def __HHActionNoop():
+def HHActionNoop():
     pass
 
 class BaseState():
-    def __init__(self, name, sid, superId, trans, inits=None):
+    def __init__(self, name, sid, superId, trans, subst=None):
         self.__name = name
         self.__id = sid
         self.__superId = superId
         self.__trans = trans
-        self.__inits = inits
+        self.__subst = subst
 
     def __str__(self):
         return self.__name
 
-    def enter(self, start):
+    def enter(self, start, states, entries):
         if start != self.__superId:
-            __hh_states[self.__superId].enter(start)
-        __hh_entries.get(self.__id, __HHActionNoop)()
+            states[self.__superId].enter(start, states, entries)
+        entries.get(self.__id, HHActionNoop)()
 
-    def exit(self, stop):
-        __hh_exits.get(self.__id, __HHActionNoop)()
+    def exit(self, stop, states, exits):
+        exits.get(self.__id, HHActionNoop)()
         if stop != self.__superId:
-            __hh_states[self.__superId].exit(stop)
+            states[self.__superId].exit(stop, states, exits)
 
-    def matchTransition(self, e):
+    def matchTransition(self, e, states, guards):
         trs = self.__trans.get(e, [[-1, -1, -1]])
         for tr in trs:
-            if __hh_guards.get(tr[0], __HHGuardFalse)():
+            if guards.get(tr[0], HHGuardTrue)():
                 return tr
         if self.__superId != -1:
-            return __hh_states[self.__superId].matchTransition(e)
+            return states[self.__superId].matchTransition(e, states, guards)
         return None
 
-    def init(self):
-        if self.__inits is None:
+    def init(self, states, inits, entries):
+        if self.__subst is None:
             return self.__id
-        for st in self.__inits:
-            __hh_inits.get(st, __HHActionNoop)()
-            __hh_states[st].enter(self.__id)
-            return __hh_states[st].init()
+        for st in self.__subst:
+            inits.get(st, HHActionNoop)()
+            states[st].enter(self.__id, states, entries)
+            return states[st].init(states, inits, entries)
 
-    def on(self, e):
-        tr = self.matchTransition(e)
+    def on(self, e, states, entries, exits, guards, actions, inits):
+        tr = self.matchTransition(e, states, guards)
         if tr is None:
             return -1
-        self.exit(tr[2])
-        __hh_actions.get(tr[0], __HHActionNoop)()
-        __hh_states[tr[1]].enter(tr[2])
-        return __hh_states[tr[1]].init()
+        self.exit(tr[2], states, exits)
+        actions.get(tr[0], HHActionNoop)()
+        states[tr[1]].enter(tr[2], states, entries)
+        return states[tr[1]].init(states, inits, entries)
